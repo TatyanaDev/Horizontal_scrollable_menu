70230f0ea379d49ea31a616564f3c8b92a508e18
Tatyana404 <kot.kate2020@gmail.com>
2024-02-06 12:28:56 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App";
+import "./index.css";
+
+const { render } = createRoot(document.getElementById("root") as HTMLElement);
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
